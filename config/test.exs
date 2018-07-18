f07b32ce1edd825989fbd11d9f8e814087c7d201
Ed Saunders <saunders.ed@gmail.com>
2018-07-18 22:41:12 +0100

    Remove local test config

--- config/test.exs
+++ config/test.exs
@@ -4,12 +4,8 @@ config :dataloader, Dataloader.TestRepo,
   hostname: "localhost",
   database: "dataloader_test",
   adapter: Ecto.Adapters.Postgres,
-  pool: Ecto.Adapters.SQL.Sandbox,
-  username: "nested",
-  password: "nested",
-  port: 54321
+  pool: Ecto.Adapters.SQL.Sandbox
 
-config :dataloader,
-  ecto_repos: [Dataloader.TestRepo]
+config :dataloader, ecto_repos: [Dataloader.TestRepo]
 
 config :logger, level: :warn
